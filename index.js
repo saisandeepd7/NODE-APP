2d7a56e613dc7ae1f1fae8b4c0d34e047a9c0300
saisandeepd7 <dsandeep3690@gmail.com>
2022-07-14 13:44:04 +0530

    crud

--- index.js
+++ index.js
@@ -7,14 +7,14 @@
 // import {moviesRouter} from "./routes/movies.js";
 // import cors from "cors";
  dotenv.config();
-
+ 
  const app = express();
 const PORT= 4000;
 // const movies=[
 
 //     {
 //     "id": "100",
-//     "name": "RRR",
+//     "name": "RRR", 
 //     "poster":
 //     "https://englishtribuneimages.blob.core.windows.net/gallary-content/2021/6/Desk/2021_6$largeimg_977224513.JPG",
 //     "rating": 8.8,
@@ -101,8 +101,11 @@ app.get('/', function (req, res) {
   res.send('Hello, Welcome to the APP')
 })
 app.get("/movies",async function(req,res){
-  const movies= await client.db("NODE-APP").collection("movies").find({}).toArray()
-  console.log(movies)
+  if(req.query.rating){
+    req.query.rating=+req.query.rating
+  }
+  const movies= await client.db("NODE-APP").collection("movies").find(req.query).toArray()
+  
  res.send(movies)
 })
 app.get("/movies/:id",async function(req,res){
@@ -123,5 +126,29 @@ app.get("/movies/:id",async function(req,res){
    res.send(result)
  })
 
-//cursor - Pagination | cursor --> Array | toArray()
+ app.delete("/movies/:id",async function(req,res){
+  const { id }=req.params
+  console.log(req.params,id)
+ 
+
+  const result= await client.db("NODE-APP").collection("movies").deleteOne({id:id})
+
+  console.log(result)
+  result.deletedCount > 0
+  ? res.send(("Movie Deleted"))
+  : res.status(404).send(("Movie not found"))
+ }) 
+
+ app.put("/movies/:id",async function(req,res){
+  const { id }=req.params
+  console.log(req.params,id)
+ const data=req.body
+
+  const result= await client.db("NODE-APP").collection("movies").updateOne({id:id},{$set:data})
+  res.send(result)
+  
+ })
+
+
+ //cursor - Pagination | cursor --> Array | toArray()
 app.listen(PORT,()=>console.log("App started in port number::",PORT));
